4d659f6b156a8681d05e1ed2b57318e5b80d7f04
Mizaoi <yakinlukmanc@gmail.com>
2026-04-15 15:21:44 +0700

    Delete pages/profil.php

--- pages/profil.php
+++
@@ -1,2 +0,0 @@
-<h2>Tentang Tancak Panti</h2>
-<p>Tancak Panti terletak di lereng Gunung Argopuro, Jember. Terkenal dengan air terjunnya yang jernih dan jalur trekking yang menembus perkebunan kopi rakyat.</p>
